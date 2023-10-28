6c8b367b98075c5b762343c347968d0abe33aa0f
Saulo <saulo.ftu@gmail.com>
2023-10-27 22:56:46 -0300

    first commit

---
+++ Projeto_TDSPZ_WEB-main/src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import Menu from "../Componentes/Menu";
+import { TitlePage } from "../Estilos/Estilo";
+
+
+function Home (){
+    return(
+        <>
+            <Menu />
+            <hr/>
+            <TitlePage>Home</TitlePage>
+
+           </>
+    )
+}
+
+export default Home;
---
+++ Projeto_TDSPZ_WEB-main/src/Pages/Sobre.jsx
@@ -0,0 +1,13 @@
+import Menu from "../Componentes/Menu";
+
+
+function Sobre (){
+    return(
+        <>
+            <Menu />
+            <hr/>
+            <h1>SOBRE</h1>
+        </>
+    )
+}
+export default Sobre;
